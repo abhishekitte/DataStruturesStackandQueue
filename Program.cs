d720f247b2e0fc037e31ae3c4d4cad89fb1f31f6
ABHI <abhishekitte11@gmail.com>
2021-09-22 21:53:54 +0530

    [ABHISHEK] Added a Stack using custom linked list

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresStackandQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinkedList < int > list= new LinkedList<int>();
+            //list.AddLast(56);
+            //list.AddLast(30);
+            //list.AddLast(70);
+
+            LinkedStack stack = new LinkedStack();
+            stack.LinkPush(70);
+            stack.LinkPush(30);
+            stack.LinkPush(56);
+            stack.LinkStackDisplay();
+            //stack.Peek();
+            //stack.Pop();
+            //stack.IsEmpty();
+            //CustomLinkedList obj1 = new CustomLinkedList();
+            //obj1.InsertLast(70);
+            //obj1.InsertLast(56);
+            //obj1.InsertLast(30);
+            //obj1.Display();
+            Console.ReadLine();
+        }
+    }
+}
