4495e6114ce3f0507dee5f61b915c55114dc597b
ABHI <abhishekitte11@gmail.com>
2021-09-22 22:44:12 +0530

    [ABHISHEK] delete element in queue

--- LinkQueue.cs
+++ LinkQueue.cs
@@ -8,54 +8,55 @@ namespace DataStructuresStackandQueue.cs
 {
     class LinkQueue
     {
-        CustomLinkedList list1 = new CustomLinkedList();
         Node head;
-        //public void Enqueue(int new_data)
-        //{
-        //    Node new_node = new Node(new_data);
-        //    if (this.head==null)
-        //    {
-        //        this.head = new_node;
-        //    }
-        //    else
-        //    {
-        //        Node temp = this.head;
-        //        while(temp.next!=null)
-        //        {
-        //            temp = temp.next;
-        //        }
-        //        temp.next = new_node;
-        //    }
-        //    Console.WriteLine("Inserted into List" +new_node.data);
-        //}
-
-        //method for adding element in queue using customlinkedlist
-        public void LinkEnqueue(int new_data)
+        //method for insert element in queue
+        public void Enqueue(int new_data)
         {
-            list1.InsertLast(new_data);
+            Node new_node = new Node(new_data);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+            }
+            Console.WriteLine("Inserted into List" + new_node.data);
         }
-
-        public void LinkQueueDisplay()
+        public void Dequeue()
         {
-            list1.Display();
+            //if queue is empty,return null
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            //Store previous head, and move head pointer one step ahead
+            Node temp = this.head;
+            this.head = this.head.next;
+            Console.WriteLine("Deleated element is:" + temp.data);
         }
-
         //Display method for queue
-        //public void Display()
-        //{
-        //    ///checking if stack is not empty
-        //    ///printing values till temp point towards null i.e. end of stack 
-        //    Node temp = this.head;
-        //    if (temp == null)
-        //    {
-        //        Console.WriteLine("Queue is empty");
-        //        return;
-        //    }
-        //    while (temp != null)
-        //    {
-        //        Console.WriteLine("Value in the queue : " + temp.data);
-        //        temp = temp.next;
-        //    }
-        //}
+        public void Display()
+        {
+            ///checking if stack is not empty
+            ///printing values till temp point towards null i.e. end of stack 
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine("Value in the queue : " + temp.data);
+                temp = temp.next;
+            }
+        }
     }
 }
